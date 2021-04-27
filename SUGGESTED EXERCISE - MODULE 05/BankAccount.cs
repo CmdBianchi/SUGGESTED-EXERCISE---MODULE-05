93f9c1b77b4444af0419c0b28bbdd38843ea4e43
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-27 18:43:58 -0300

    Adicionar arquivos de projeto.

---
+++ SUGGESTED EXERCISE - MODULE 05/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SUGGESTED_EXERCISE___MODULE_05{
+    //------------------------------- START -------------------------------//
+    class BankAccount {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        //------------------------- CONSTRUCTORS ------------------------- //
+        public BankAccount(int number, string holder) {
+            Number = number;
+            Holder = holder;
+        }
+        public BankAccount(int number, string holder, double balance) : this(number, holder) {
+            Balance = balance;
+        }
+        //------------------------- CLASS |  MET --------------------------//
+        public void Deposit(double deposit) {
+            Balance += deposit;
+        }
+        public void Withdraw(double deposit) {
+            Balance -= deposit + 5.0;
+        }
+        public override string ToString() {
+            return "Conta "
+            + Number + ", Titular: " + Holder + ", Saldo: $" + Balance.ToString("F2");
+        }       
+    }
+    //-------------------------------- END -------------------------------//
+}
---
+++ SUGGESTED EXERCISE - MODULE 05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace SUGGESTED_EXERCISE___MODULE_05 {
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            BankAccount bankAccount;
+
+            Console.Write("Entre o número da conta: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string holder = Console.ReadLine();
+            Console.Write("Haverá depósito inicial SIM ou NÃO ? (s/n)");
+            char answer = char.Parse(Console.ReadLine());
+
+            if(answer == 's' || answer == 'S') {
+                Console.WriteLine("Entre com o valor do depósito inicial: ");
+                double initialdeposit = double.Parse(Console.ReadLine());
+                bankAccount = new BankAccount(number, holder, initialdeposit);
+            }
+            else {
+                bankAccount = new BankAccount(number, holder);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(bankAccount);
+
+            Console.WriteLine();
+            Console.Write("Entre um valorr para depósito: ");
+            double amount = double.Parse(Console.ReadLine());
+            bankAccount.Deposit(amount);
+            Console.WriteLine("Dados da conta att: ");
+            Console.WriteLine(bankAccount);
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
